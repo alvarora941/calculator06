ad059df1166776077ea9033a4066c254214a2c5f
Alvaro Roces <alvarora94@educastur.es>
2024-04-18 14:27:07 +0000

    Añadido esqueleto Spring boot

---
+++ src/main/java/com/ciber06/wargames06/Wargames06Application.java
@@ -0,0 +1,13 @@
+package com.ciber06.wargames06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wargames06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wargames06Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciber06/wargames06/Wargames06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber06.wargames06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wargames06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
